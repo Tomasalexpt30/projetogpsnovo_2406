58f5d653893207349325925a21919646abe15fe7
Tomasalex30 <120526991+Tomasalex30@users.noreply.github.com>
2025-07-09 21:34:03 +0100

    Novos Beacons - Configuração APP + Codigo

--- lib/pages/navigation/navigation_manager.dart
+++ lib/pages/navigation/navigation_manager.dart
@@ -25,11 +25,55 @@ class BeaconInfo {
       uuid.hashCode ^ major.hashCode ^ minor.hashCode ^ macAddress.toUpperCase().hashCode;
 }
 
+// Piso -1 = 10000
+// Piso 0 = 10001
+// Piso 1 = 10002
+// Piso 2 = 10003
+// Piso 3 = 10004
+// Piso 4 = 10005
+// uuid 107e0a1390f342bfb980181d93c3ccd2
+// pw aa14061112
+
 class NavigationManager {
   final Map<BeaconInfo, String> beaconLocations = {
-    BeaconInfo('fda50693-a4e2-4fb1-afcf-c6eb07647825', 10001, 1, 'FF:87:6D:60:E2:CE'): 'Beacon 1',
-    BeaconInfo('fda50693-a4e2-4fb1-afcf-c6eb07647825', 10001, 2, 'DC:ED:EA:6E:0B:2D'): 'Beacon 3',
-    BeaconInfo('fda50693-a4e2-4fb1-afcf-c6eb07647825', 10001, 3, 'F2:29:76:B1:E1:4D'): 'Beacon 15',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 1, 'FF:87:6D:60:E2:CE'): 'Beacon 1',
+    //BeaconInfo('', 1, 2, ''): 'Beacon 2',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 3, 'F2:29:76:B1:E1:4D'): 'Beacon 3',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 4, 'FD:F3:6B:A2:22:DD'): 'Beacon 4',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 5, 'EC:3B:82:29:F8:2D'): 'Beacon 5',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 6, 'FA:93:62:AF:62:66'): 'Beacon 6',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10000, 7, 'E8:C5:D8:A6:E9:BA'): 'Beacon 7',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 8, 'C3:8B:3B:92:A0:09'): 'Beacon 8',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 9, 'DD:82:40:D0:83:6F'): 'Beacon 9',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10000, 10, 'DD:28:73:88:D8:D2'): 'Beacon 10',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10000, 11, 'E5:12:43:87:BE:A5'): 'Beacon 11',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10000, 12, 'C4:DF:3A:76:9C:CD'): 'Beacon 12',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10000, 13, 'D6:71:FD:D8:53:A1'): 'Beacon 13',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10000, 14, 'FE:FD:80:74:9B:00'): 'Beacon 14',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 15, 'C2:C7:83:CD:77:6E'): 'Beacon 15',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 16, 'F8:BE:75:76:4D:29'): 'Beacon 16',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 17, 'C6:97:47:5A:8F:6C'): 'Beacon 17',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10000, 18, 'E3:1D:5F:D5:56:35'): 'Beacon 18',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10002, 19, 'F5:4B:CF:31:D5:A3'): 'Beacon 19',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10002, 20, 'F5:71:BD:7A:B9:A3'): 'Beacon 20',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10003, 21, 'C8:77:BE:A3:90:64'): 'Beacon 21',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10001, 22, 'E2:56:6F:02:0A:DD'): 'Beacon 22',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10002, 23, 'C7:68:90:86:BE:7B'): 'Beacon 23',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10002, 24, 'C3:EE:4E:EC:04:FF'): 'Beacon 24',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10002, 25, 'DB:DB:2C:2A:42:D4'): 'Beacon 25',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10003, 26, 'CA:EA:BC:7D:9E:E8'): 'Beacon 26',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10002, 27, 'CB:DD:F2:E9:BD:53'): 'Beacon 27',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10002, 28, 'E0:62:65:E0:D4:FB'): 'Beacon 28',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10002, 29, 'FF:B2:A4:E8:07:2B'): 'Beacon 29',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10002, 30, 'EB:9E:2E:26:D8:54'): 'Beacon 30',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10003, 31, 'F6:AD:B9:EA:40:63'): 'Beacon 31',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10003, 32, 'FE:31:74:78:F8:C9'): 'Beacon 32',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10003, 33, 'EB:C0:32:AE:59:7F'): 'Beacon 33',
+    //BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10004, 34, ''): 'Beacon 34',
+    //BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10005, 35, ''): 'Beacon 35',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10003, 36, 'DC:ED:EA:6E:0B:2D'): 'Beacon 36',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10004, 37, 'C5:14:B9:27:15:D0'): 'Beacon 37',
+    BeaconInfo('107e0a13-90f3-42bf-b980-181d93c3ccd2', 10003, 38, 'E1:AD:34:83:56:08'): 'Beacon 38',
   };
 
   Map<String, Map<String, int>> mapaFaculdade = {};
